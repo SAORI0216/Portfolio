dc008f58cd41525c691b4a014d06ecc2a8829a5b
SAORI0216 <sa.06att.f@gmail.com>
2026-02-20 12:33:58 +0900

    フッターにSNSへのリンクを実装

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,10 +1,55 @@
+import { FaGithub,FaXTwitter } from "react-icons/fa6";
+import { SiZenn } from "react-icons/si";
+
 export default function Footer(){
   return( 
   <footer className="w-full bg-[#cb8967]"> 
-    <div className="mx-auto max-w-5xl py-4 text-center"> 
+    <div className="mx-auto max-w-5xl py-6 text-center space-y-4">
+      {/* SNSアイコン */}
+      <div className="flex justify-center gap-6 text-white text-xl">
+        <a
+         href="https://github.com/SAORI0216"
+         target="_blank"
+         rel="noopener noreferrer"
+         className="hover:opacity-70 transition"
+        >
+          <FaGithub />
+        </a>
+        <a
+         href="https://zenn.dev/saorinn"
+         target="_blank"
+         rel="noopener noreferrer"
+         className="hover:opacity-70 transition"
+        >
+          <SiZenn />
+        </a>
+        <a
+         href="https://x.com/0216saorinn"
+         target="_blank"
+         rel="noopener noreferrer"
+         className="hover:opacity-70 transition"
+        >
+          <FaXTwitter />
+        </a>
+
+      </div>
+
+      {/* コピーライト */}
       <p className="text-sm text-white">
         © {new Date().getFullYear()} Saori Portfolio
       </p> 
+      {/* クレジット */}
+      <p className="text-xs text-white/80">
+        Icon by{" "}
+        <a
+         href="https://icons8.com"
+         target="_blank"
+         rel="noopener noreferrer"
+         className="underline hover:opacity-80"
+        >
+          Icons8
+        </a>
+      </p>
     </div> 
   </footer> 
   ); 
