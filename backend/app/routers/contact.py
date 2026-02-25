f1d81e539e01b7d8e031898d0745fcffd60cd07b
SAORI0216 <sa.06att.f@gmail.com>
2026-02-25 10:09:55 +0900

    add:Contactルーター作成

---
+++ backend/app/routers/contact.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.models.contact import Contact
+from app.schemas.contact import ContactCreate,ContactResponse
+from typing import List
+
+router = APIRouter(prefix="/contacts",tags=["contacts"])
+
+@router.post("/",response_model=ContactResponse)
+def create_contact(contact:ContactCreate,db:Session=Depends(get_db)):
+    new_contact = Contact(
+        name=contact.name,
+        email=contact.email,
+        message=contact.message,
+        status="unhandled"
+        )
+    db.add(new_contact)
+    db.commit()
+    db.refresh(new_contact)
+    return new_contact
+
+@router.get("/",response_model=List[ContactResponse])
+def get_contacts(db:Session=Depends(get_db)):
+    return db.query(Contact).order_by(Contact.created_at.desc()).all()
+
+@router.get("/{contact_id}",response_model=ContactResponse)
+def get_contact(contact_id:int,db:Session=Depends(get_db)):
+    return db.query(Contact).filter(Contact.id == contact_id).first()
+    
