ce000e8605d5268a366af7d7ba28c12203d69ce8
SAORI0216 <sa.06att.f@gmail.com>
2026-03-02 11:24:04 +0900

    worksを編集できるように実装

--- backend/app/routers/works.py
+++ backend/app/routers/works.py
@@ -1,11 +1,41 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.models.work import Work
-from app.schemas.work import WorkRead
+from app.schemas.work import WorkRead,WorkCreate,WorkUpdate
 
 router = APIRouter(prefix="/works",tags=["works"])
 
 @router.get("",response_model=list[WorkRead])
 def get_works(db:Session=Depends(get_db)):
     return db.query(Work).all()
+
+@router.post("",response_model=WorkRead)
+def create_work(work:WorkCreate,db:Session=Depends(get_db)):
+    new_work = Work(**work.model_dump())
+    db.add(new_work)
+    db.commit()
+    db.refresh(new_work)
+    return new_work
+
+@router.put("/{work_id}",response_model=WorkUpdate)
+def update_work(work_id:int,work:WorkUpdate,db:Session=Depends(get_db)):
+    db_work = db.query(Work).filter(Work.id == work_id).first()
+    if not db_work:
+        raise HTTPException(status_code=404,detail="Work not found")
+    
+    for key,value in work.model_dump().items():
+        setattr(db_work,key,value)
+    db.commit()
+    db.refresh(db_work)
+    return db_work
+
+@router.delete("/{work_id}")
+def delete_work(work_id:int,db:Session=Depends(get_db)):
+    db_work = db.query(Work).filter(Work.id == work_id).first()
+    if not db_work:
+        raise HTTPException(statue_code=404,detail="Work not found")
+    
+    db.delete(db_work)
+    db.commit()
+    return{"message":"Deleted successfully!"}
--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -9,4 +10,5 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
